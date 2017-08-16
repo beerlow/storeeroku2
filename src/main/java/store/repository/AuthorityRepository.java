d2ab964e876ec542300316b7b9081fff2c2e8610
romain <romain@MacBook-Pro-de-romain.local>
2017-08-16 11:07:00 +0200

    first commit

---
+++ src/main/java/store/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package store.repository;
+
+import store.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
