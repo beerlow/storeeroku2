284bd331ce0b5a26387f7f4b5f5ffced2350f7a7
romain <romain@MacBook-Pro-de-romain.local>
2017-08-16 22:34:57 +0200

    correction login facebook

--- src/main/java/store/service/SocialService.java
+++ src/main/java/store/service/SocialService.java
@@ -4,7 +4,8 @@ import store.domain.Authority;
 import store.domain.User;
 import store.repository.AuthorityRepository;
 import store.repository.UserRepository;
-
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -20,6 +21,8 @@ import java.util.HashSet;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.Set;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class SocialService {
@@ -126,5 +129,35 @@ public class SocialService {
     private void createSocialConnection(String login, Connection<?> connection) {
         ConnectionRepository connectionRepository = usersConnectionRepository.createConnectionRepository(login);
         connectionRepository.addConnection(connection);
+    }
+    @PostConstruct
+    private void init() {
+        try {
+            String[] fieldsToMap = { "id", "about", "age_range", "birthday",
+                    "context", "cover", "currency", "devices", "education",
+                    "email", "favorite_athletes", "favorite_teams",
+                    "first_name", "gender", "hometown", "inspirational_people",
+                    "installed", "install_type", "is_verified", "languages",
+                    "last_name", "link", "locale", "location", "meeting_for",
+                    "middle_name", "name", "name_format", "political",
+                    "quotes", "payment_pricepoints", "relationship_status",
+                    "religion", "security_settings", "significant_other",
+                    "sports", "test_group", "timezone", "third_party_id",
+                    "updated_time", "verified", "viewer_can_send_gift",
+                    "website", "work" };
+
+            Field field = Class.forName(
+                    "org.springframework.social.facebook.api.UserOperations")
+                    .getDeclaredField("PROFILE_FIELDS");
+            field.setAccessible(true);
+
+            Field modifiers = field.getClass().getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            field.set(null, fieldsToMap);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
